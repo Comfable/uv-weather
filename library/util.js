8507ad09aad57be795f1a2efbedf48294103d3f3
Ali Monam <ali@comfable.com>
2021-02-09 16:03:58 -0500

    1.2.23.02

--- library/util.js
+++ library/util.js
@@ -979,7 +979,7 @@ function animatedBadge() {
 function largBadgeNumber(displayNumber, lightBadge) {
   var canvas = new OffscreenCanvas(100, 100);
   var ctx = canvas.getContext("2d");
-  ctx.font = "bold 18px Helvetica, Arial";
+  ctx.font = "normal 18px Arial";
   // if (lightBadge == 1) {
   //   ctx.fillStyle = "white";
   // } else {
